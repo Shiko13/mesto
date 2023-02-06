fe01ede34538e09f48a75c2c74f9daab78c8ef05
Sergey <shikofreid@yandex.ru>
2023-02-06 23:38:37 +0400

    Some fixes after first review

--- scripts/index.js
+++ scripts/index.js
@@ -24,34 +24,6 @@ const ZoomImage = document.querySelector('.zoom__image');
 const ZoomTitle = document.querySelector('.zoom__title');
 const popupCloseButtonZoom = popupZoomCard.querySelector(".popup__close-button");
 
-const elements = document.querySelector(".elements");
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
-
 function openPopupEdit() {
   popupInputTitleEdit.value = profileTitle.textContent;
   popupInputSubtitleEdit.value = profileSubtitle.textContent;
@@ -85,27 +57,13 @@ function handleFormEditSubmit(evt) {
   closePopupEdit();
 }
 
-function handleFormClick(evt) {
-  if (evt.target.classList.contains("element__delete-button")) {
-    evt.target.closest(".element").remove(); }
-  else if (evt.target.classList.contains("element__like-button")) {
-      evt.target.classList.toggle("element__like-button_active");
-  } else if (evt.target.classList.contains('element__image')) {
-    const cardTitle = evt.target.closest('.element').querySelector('.element__title');
-    ZoomImage.src = evt.target.src;
-    ZoomImage.alt = evt.target.alt;
-    ZoomTitle.textContent = cardTitle.textContent;
-    openPopupZoom(popupZoomCard);
-  }
-}
-
 function handleFormAddSubmit(evt) {
   evt.preventDefault();
   const card = {
     name: popupInputTitleAdd.value,
     link: popupInputSubtitleAdd.value,
   }
-  renderCard(addCard(card));
+  renderCard(card);
   closePopupAddCard(evt);
   resetForm(evt.target);
 }
@@ -115,6 +73,20 @@ function addCard(elem) {
   const cloneCardTitle = cloneCard.querySelector(".element__title");
   const cloneCardImage = cloneCard.querySelector(".element__image");
 
+  cloneCard.querySelector('.element__delete-button').addEventListener('click', function (evt) {
+    evt.target.parentElement.remove()
+  });
+  cloneCard.querySelector(".element__like-button").addEventListener("click", function (evt) {
+    evt.target.classList.toggle("element__like-button_active")
+  });
+  cloneCard.querySelector('.element__image').addEventListener('click', function (evt) {
+    const cardTitle = evt.target.closest('.element').querySelector('.element__title'); 
+    ZoomImage.src = evt.target.src; 
+    ZoomImage.alt = evt.target.alt; 
+    ZoomTitle.textContent = cardTitle.textContent; 
+    openPopupZoom(popupZoomCard); 
+  });
+
   cloneCardImage.src = elem.link;
   cloneCardImage.alt = `"${elem.name}"`;
   cloneCardTitle.textContent = elem.name;
@@ -122,13 +94,14 @@ function addCard(elem) {
   return cloneCard;
 }
 
-function renderCard(elem = addCard(elems)) {
-  cards.prepend(elem);
+function renderCard(cardData) {
+  const card = addCard(cardData);
+  cards.prepend(card);
 }
 
 function fillCards(initCards) {
   initCards.forEach((elem) => {
-    renderCard(addCard(elem));
+    renderCard(elem);
   });
 }
 
@@ -143,8 +116,6 @@ profileAddButton.addEventListener("click", openPopupAdd);
 popupCloseButtonEdit.addEventListener("click", closePopupEdit);
 popupCloseButtonAdd.addEventListener("click", closePopupAddCard);
 popupCloseButtonZoom.addEventListener("click", closePopupZoom);
-cards.addEventListener("click", handleFormClick);
 popupEditForm.addEventListener("submit", handleFormEditSubmit);
 popupAddCard.addEventListener("submit", handleFormAddSubmit);
-
 
